7e9649ff1345b6fc495f4581ec06a6b3c2bae03b
VSEdwardCarter <edward.j.carter2@gamil.com>
2021-09-20 14:41:52 -0500

    Carter and Kyle Solution

--- src/main/java/com/SpringPlayground_2/FlightsController.java
+++ src/main/java/com/SpringPlayground_2/FlightsController.java
@@ -1,4 +1,136 @@
 package com.SpringPlayground_2;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.PortUnreachableException;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/flights")
 public class FlightsController {
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    static class Person{
+        private String firstName;
+        private String lastName;
+
+        @JsonProperty("FirstName")
+        public String getFirstName() {
+            return firstName;
+        }
+
+        public void setFirstName(String firstName) {
+            this.firstName = firstName;
+        }
+
+        @JsonProperty("LastName")
+        public String getLastName() {
+            return lastName;
+        }
+
+        public void setLastName(String lastName) {
+            this.lastName = lastName;
+        }
+    }
+
+
+    static class Ticket{
+        private Person passenger ;
+        private double price;
+
+        public Ticket(Person passenger, double price){
+            this.passenger = passenger;
+            this.price = price;
+
+        }
+
+        @JsonProperty("Passenger")
+        public Person getPassenger() {
+            return passenger;
+        }
+
+        @JsonProperty("Price")
+        public double getPrice() {
+            return price;
+        }
+    }
+
+    static class Flight{
+        ArrayList<Ticket> tickets;
+        Calendar departs;
+
+        public Flight(Calendar departs){
+            this.departs = departs;
+            tickets = new ArrayList<>();
+
+        }
+        @JsonProperty("Tickets")
+        public ArrayList<Ticket> getTickets() {
+            return tickets;
+        }
+
+        public void addTicket(Ticket ticket){
+            tickets.add(ticket);
+        }
+
+
+        @JsonProperty("Departs")
+        public String getDepartsInString() {
+            return departs.get(Calendar.YEAR) +"-" + departs.get(Calendar.MONTH)
+                    + "-" + departs.get(Calendar.DATE) + " " + departs.get(Calendar.HOUR_OF_DAY)
+                    + ":" + departs.get(Calendar.MINUTE);
+
+        }
+    }
+
+    @GetMapping("/flight")
+    public Flight getFlight(){
+        return getTestFlight();
+    }
+
+    private Flight getTestFlight() {
+        Person john = new Person();
+        john.setFirstName("John");
+        john.setLastName("Wilson");
+
+        Calendar departs = Calendar.getInstance();
+        departs.set(2021,8,01);
+
+
+        Flight testflight = new Flight(departs);
+        testflight.addTicket(new Ticket(john, 400));
+        return  testflight;
+    }
+
+    @GetMapping
+    public List<Flight> getFlights(){
+        List<Flight> flights = new ArrayList<>();
+
+        Person david = new Person();
+        david.setFirstName("David");
+
+        Calendar departs1 = Calendar.getInstance();
+        departs1.set(2021,8, 2);
+
+
+        Flight testflight1 = new Flight(departs1);
+
+        testflight1.addTicket(new Ticket(david, 200));
+        flights.add(getTestFlight());
+        flights.add(testflight1);
+
+        return flights;
+    }
+
+
+
+
 }
--- src/main/java/com/SpringPlayground_2/JSONRseponseController.java
+++ src/main/java/com/SpringPlayground_2/JSONRseponseController.java
@@ -1,125 +1,125 @@
-package com.SpringPlayground_2;
-
-
-import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonProperty;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.*;
-import java.time.format.DateTimeFormatter;
-import java.time.LocalDateTime;
-
-@RestController
-@RequestMapping("/json")
-public class JSONRseponseController {
-
-
-
-       @GetMapping("/person")
-        public Person getPerson() {
-            Person person = new Person();
-            person.firstName = "Dwayne";
-            person.lastName = "Johnson";
-            person.setPassengerID(6);
-            return person;
-        }
-
-        public static class Person {
-            private String firstName;
-            private String lastName;
-            private int passengerID;
-
-            public String getFirstName() {
-                return firstName;
-            }
-
-            public int getPassengerID(){
-                return passengerID;
-            }
-
-            public void setPassengerID(int passengerID) {
-                this.passengerID = passengerID;
-            }
-
-            public void setFirstName(String firstName) {
-                this.firstName = firstName;
-            }
-
-            public String getLastName() {
-                return lastName;
-            }
-
-            public void setLastName(String lastName) {
-                this.lastName = lastName;
-            }
-        }
-
-        @GetMapping("/people")
-        public List<Person> getPeople(){
-           Person person1 = new Person();
-           person1.setFirstName("Edward");
-           person1.setLastName("Carter");
-           person1.setPassengerID(1);
-
-           Person person2 = new Person();
-           person2.setFirstName("Kelesego");
-           person2.setLastName("Carter");
-           person2.setPassengerID(2);
-
-           Person person3 = new Person();
-           person3.setFirstName("Kenako");
-           person3.setLastName("Carter");
-           person3.setPassengerID(3);
-
-           return Arrays.asList(person1,person2,person3);
-        }
-
-        class SuitCase{
-           private final int width;
-           private final int height;
-           private final int depth;
-           private final int weight;
-
-           @JsonCreator
-            SuitCase(
-                    @JsonProperty("width") int width,
-                    @JsonProperty("height") int height,
-                    @JsonProperty("depth") int depth,
-                    @JsonProperty("weight") int weight
-           ){
-               this.width = width;
-               this.height = height;
-               this.depth = depth;
-               this.weight = weight;
-           }
-
-            public int getWidth() {
-                return width;
-            }
-
-            public int getHeight() {
-                return height;
-            }
-
-            public int getDepth() {
-                return depth;
-            }
-
-            public int getWeight() {
-                return weight;
-            }
-        }
-
-        @GetMapping("/suitcase")
-        public SuitCase getSuitcase(){
-           SuitCase a = new SuitCase(12,18,8, 35);
-
-           return a;
-        }
-
-
-
-
-}
+//package com.SpringPlayground_2;
+//
+//
+//import com.fasterxml.jackson.annotation.JsonCreator;
+//import com.fasterxml.jackson.annotation.JsonProperty;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//import java.util.*;
+//import java.time.format.DateTimeFormatter;
+//import java.time.LocalDateTime;
+//
+//@RestController
+//@RequestMapping("/json")
+//public class JSONRseponseController {
+//
+//
+//
+//       @GetMapping("/person")
+//        public Person getPerson() {
+//            Person person = new Person();
+//            person.firstName = "Dwayne";
+//            person.lastName = "Johnson";
+//            person.setPassengerID(6);
+//            return person;
+//        }
+//
+//        public static class Person {
+//            private String firstName;
+//            private String lastName;
+//            private int passengerID;
+//
+//            public String getFirstName() {
+//                return firstName;
+//            }
+//
+//            public int getPassengerID(){
+//                return passengerID;
+//            }
+//
+//            public void setPassengerID(int passengerID) {
+//                this.passengerID = passengerID;
+//            }
+//
+//            public void setFirstName(String firstName) {
+//                this.firstName = firstName;
+//            }
+//
+//            public String getLastName() {
+//                return lastName;
+//            }
+//
+//            public void setLastName(String lastName) {
+//                this.lastName = lastName;
+//            }
+//        }
+//
+//        @GetMapping("/people")
+//        public List<Person> getPeople(){
+//           Person person1 = new Person();
+//           person1.setFirstName("Edward");
+//           person1.setLastName("Carter");
+//           person1.setPassengerID(1);
+//
+//           Person person2 = new Person();
+//           person2.setFirstName("Kelesego");
+//           person2.setLastName("Carter");
+//           person2.setPassengerID(2);
+//
+//           Person person3 = new Person();
+//           person3.setFirstName("Kenako");
+//           person3.setLastName("Carter");
+//           person3.setPassengerID(3);
+//
+//           return Arrays.asList(person1,person2,person3);
+//        }
+//
+//        class SuitCase{
+//           private final int width;
+//           private final int height;
+//           private final int depth;
+//           private final int weight;
+//
+//           @JsonCreator
+//            SuitCase(
+//                    @JsonProperty("width") int width,
+//                    @JsonProperty("height") int height,
+//                    @JsonProperty("depth") int depth,
+//                    @JsonProperty("weight") int weight
+//           ){
+//               this.width = width;
+//               this.height = height;
+//               this.depth = depth;
+//               this.weight = weight;
+//           }
+//
+//            public int getWidth() {
+//                return width;
+//            }
+//
+//            public int getHeight() {
+//                return height;
+//            }
+//
+//            public int getDepth() {
+//                return depth;
+//            }
+//
+//            public int getWeight() {
+//                return weight;
+//            }
+//        }
+//
+//        @GetMapping("/suitcase")
+//        public SuitCase getSuitcase(){
+//           SuitCase a = new SuitCase(12,18,8, 35);
+//
+//           return a;
+//        }
+//
+//
+//
+//
+//}
