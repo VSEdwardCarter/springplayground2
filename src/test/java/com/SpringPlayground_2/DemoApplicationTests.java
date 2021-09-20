c2c6763121dcc38cc02e8612a57504dce858b821
VSEdwardCarter <edward.j.carter2@gamil.com>
2021-09-20 09:57:56 -0500

    Removed Spaceship references

---
+++ src/test/java/com/SpringPlayground_2/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SpringPlayground_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
