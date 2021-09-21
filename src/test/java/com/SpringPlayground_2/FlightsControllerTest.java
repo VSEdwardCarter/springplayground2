1b59c95d7d0a7a1000534fb00e743b9d2c0a3d97
VSEdwardCarter <edward.j.carter2@gamil.com>
2021-09-21 10:08:53 -0500

    Completed learn Content up to nested objects

--- src/test/java/com/SpringPlayground_2/FlightsControllerTest.java
+++ src/test/java/com/SpringPlayground_2/FlightsControllerTest.java
@@ -1,61 +1,61 @@
-package com.SpringPlayground_2;
-
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.http.MediaType;
-import org.springframework.test.web.servlet.MockMvc;
-
-
-import static org.hamcrest.Matchers.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static  org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-
-@WebMvcTest (FlightsController.class)
-
-public class FlightsControllerTest {
-    @Autowired
-    MockMvc mvc;
-
-    @Disabled
-    @Test
-    public void testCreatPersonJSONClass() throws Exception{
-        this.mvc.perform(
-                        get("/flights/flight")
-                                .accept(MediaType.APPLICATION_JSON)
-                                .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.tickets[0].passenger.firstName", is(notNullValue())))
-                .andExpect(jsonPath("$.tickets[0].passenger.lastName", is(notNullValue())))
-                .andExpect(jsonPath("$.tickets[0].price", is(notNullValue())))
-                .andExpect(jsonPath("$.departs", is(notNullValue())));
-    }
-
-    @Test
-    public void testListOfFlights() throws Exception{
-        this.mvc.perform(
-                        get("/flights")
-                                .accept(MediaType.APPLICATION_JSON)
-                                .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$[1].tickets[0].passenger.firstName", is(notNullValue())))
-                .andExpect(jsonPath("$[1].tickets[0].passenger",   hasProperty("lastName")))
-                .andExpect(jsonPath("$[1].tickets[0].price", is(notNullValue())));
-    }
-    @Test
-    public void testCreatPersonJSONClassWithCaps() throws Exception{
-        this.mvc.perform(
-                        get("/flights/flight")
-                                .accept(MediaType.APPLICATION_JSON)
-                                .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.Tickets[0].Passenger.FirstName", is(notNullValue())))
-                .andExpect(jsonPath("$.Tickets[0].Passenger.LastName", is(notNullValue())))
-                .andExpect(jsonPath("$.Tickets[0].Price", is(notNullValue())))
-                .andExpect(jsonPath("$.Departs", is(notNullValue())));
-    }
-
-}
-
+//package com.SpringPlayground_2;
+//
+//import org.junit.jupiter.api.Disabled;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+//import org.springframework.http.MediaType;
+//import org.springframework.test.web.servlet.MockMvc;
+//
+//
+//import static org.hamcrest.Matchers.*;
+//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+//import static  org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+//
+//@WebMvcTest (FlightsController.class)
+//
+//public class FlightsControllerTest {
+//    @Autowired
+//    MockMvc mvc;
+//
+//    @Disabled
+//    @Test
+//    public void testCreatPersonJSONClass() throws Exception{
+//        this.mvc.perform(
+//                        get("/flights/flight")
+//                                .accept(MediaType.APPLICATION_JSON)
+//                                .contentType(MediaType.APPLICATION_JSON))
+//                .andExpect(status().isOk())
+//                .andExpect(jsonPath("$.tickets[0].passenger.firstName", is(notNullValue())))
+//                .andExpect(jsonPath("$.tickets[0].passenger.lastName", is(notNullValue())))
+//                .andExpect(jsonPath("$.tickets[0].price", is(notNullValue())))
+//                .andExpect(jsonPath("$.departs", is(notNullValue())));
+//    }
+//
+//    @Test
+//    public void testListOfFlights() throws Exception{
+//        this.mvc.perform(
+//                        get("/flights")
+//                                .accept(MediaType.APPLICATION_JSON)
+//                                .contentType(MediaType.APPLICATION_JSON))
+//                .andExpect(status().isOk())
+//                .andExpect(jsonPath("$[1].tickets[0].passenger.firstName", is(notNullValue())))
+//                .andExpect(jsonPath("$[1].tickets[0].passenger",   hasProperty("lastName")))
+//                .andExpect(jsonPath("$[1].tickets[0].price", is(notNullValue())));
+//    }
+//    @Test
+//    public void testCreatPersonJSONClassWithCaps() throws Exception{
+//        this.mvc.perform(
+//                        get("/flights/flight")
+//                                .accept(MediaType.APPLICATION_JSON)
+//                                .contentType(MediaType.APPLICATION_JSON))
+//                .andExpect(status().isOk())
+//                .andExpect(jsonPath("$.Tickets[0].Passenger.FirstName", is(notNullValue())))
+//                .andExpect(jsonPath("$.Tickets[0].Passenger.LastName", is(notNullValue())))
+//                .andExpect(jsonPath("$.Tickets[0].Price", is(notNullValue())))
+//                .andExpect(jsonPath("$.Departs", is(notNullValue())));
+//    }
+//
+//}
+//
--- src/test/java/com/SpringPlayground_2/JSONResponseControllerTest.java
+++ src/test/java/com/SpringPlayground_2/JSONResponseControllerTest.java
@@ -1,33 +1,101 @@
-//package com.SpringPlayground_2;
-//
-//import org.junit.jupiter.api.Test;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-//import org.springframework.http.MediaType;
-//import org.springframework.test.web.servlet.MockMvc;
-//
-//
-//import static org.hamcrest.Matchers.is;
-//
-//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-//import static  org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-//
-//@WebMvcTest(JSONRseponseController.class)
-//
-//public class JSONResponseControllerTest {
-//    @Autowired
-//    private MockMvc mvc;
-//
-//    @Test
-//    public void testObject() throws Exception {
-//        this.mvc.perform(
-//                        get("/json/person")
-//                                .accept(MediaType.APPLICATION_JSON)
-//                                .contentType(MediaType.APPLICATION_JSON))
-//                .andExpect(status().isOk())
-//                .andExpect(jsonPath("$.firstName", is("Dwayne")))
-//                .andExpect(jsonPath("$.lastName", is("Johnson")));
-//    }
-//}
-//
+package com.SpringPlayground_2;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+
+
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.HashMap;
+
+import static org.hamcrest.Matchers.is;
+
+import static  org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+
+@WebMvcTest(JSONRseponseController.class)
+
+
+public class JSONResponseControllerTest {
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void testObject() throws Exception {
+        this.mvc.perform(
+                        get("/json/person")
+                                .accept(MediaType.APPLICATION_JSON)
+                                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.firstName", is("Dwayne")))
+                .andExpect(jsonPath("$.lastName", is("Johnson")));
+    }
+
+    @Test
+    public void testObjectParams() throws Exception{
+        MockHttpServletRequestBuilder request = post("/json/testing")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{\"q\": \"other\", \"from\": \"2010\"}");
+
+        this.mvc.perform(request)
+                .andExpect(status().isOk())
+                .andExpect(content().string("Search: q=something from =2010"));
+    }
+
+                      // 1
+
+    @Test
+    public void testCreate() throws Exception {
+        ObjectMapper objectMapper = new ObjectMapper(); // Helper
+
+        HashMap<String, Object> data = new HashMap<String, Object>(){  // 2
+            {
+                put("name", "Hercules");
+                put("age", 57);
+            }
+        };
+
+        String json = objectMapper.writeValueAsString(data);            // 3
+
+        MockHttpServletRequestBuilder request = post("/json/test2")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(json);                                         // 4
+
+        this.mvc.perform(request).andExpect(status().is4xxClientError());
+    }
+
+    @Test
+    public void testCreateSuiteCase() throws Exception{
+        ObjectMapper objectMapper = new ObjectMapper();
+        JSONRseponseController.SuitCase suitecase1 = new JSONRseponseController.SuitCase(12, 18,8,35);
+
+        String body = objectMapper.writeValueAsString(suitecase1);
+
+        MockHttpServletRequestBuilder request = post("/json/test2")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(body);
+
+        this.mvc.perform(request).andExpect(status().is4xxClientError());
+    }
+
+    @Test
+    public void sendPersonInfoWithJsonFile() throws Exception{
+        String body = getJson("/data.json");
+         this.mvc.perform(post("/json/test2")
+                 .contentType(MediaType.APPLICATION_JSON)
+                 .content(body)).andExpect(status().is4xxClientError());
+    }
+
+    private String getJson(String path) throws  Exception{
+        URL url = this.getClass().getResource(path);
+        return new String (Files.readAllBytes(Paths.get(url.getFile())));
+    }
+}
+
